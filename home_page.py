03043a5c6560442ee64f96881bb8d09a7f625623
AhmadEssamOmar1 <ahmad.essam.256@gmail.com>
2024-12-03 09:13:55 +0200

    Add files via upload

---
+++ home_page.py
@@ -0,0 +1,28 @@
+class HomePage():
+
+ link_site = "https://mcitcareerssd.elevatus.io/"
+ link_accept_cookies = "/html/body/div[3]/div/div[2]/div[1]/div[4]/button[1]"
+ second_url="https://mcitcareerssd.elevatus.io/"
+ register_button= '/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[5]/button[2]/span[1]'
+ second_url_2= "https://mcitcareerssd.elevatus.io/register"
+ rand1="input[placeholder='First name']"
+ rand2="input[placeholder='Last name']"
+ mail ="input[placeholder='Email']"
+ password="input[placeholder='Password']"
+ confirm_pass="input[placeholder='Confirm Password']"
+ checkbox1="/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[7]/label[1]"
+ checkbox2="/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[8]/button[1]"
+ veri_mail= "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=AcMMx-c__t2NppfO77Zk-ck_2H_TTkslVJKNojSNYJ8LJnP8wpy0IxYQtcRTp1EdSO2HjiGOQ6IDrA&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-468444643%3A1732976724492002&ddm=1"
+ me_mail="/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input"
+ login_button= "/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/span"
+ me_pass="/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input"
+ me_login="/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button/span"
+ word_click="/html/body/div[7]/div[3]/div/div[2]/div[4]/div/div/div/div[2]/div/div[1]/div/div[1]/div[5]/div[1]/div/table/tbody/tr[1]/td[5]/div/div/div/span/span"
+ link_site2="https://mcitcareerssd.elevatus.io/jobs"
+ view_click='/html/body/div[1]/div[4]/div/div/div/div/div/div/div[3]/button/span'
+ apply_click='/html/body/div[1]/div[3]/div/div[2]/div/div[6]/button/span'
+ calendar1_click="/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"
+ gender="/html[1]/body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[3]/div[3]/div[1]/div[1]/div[1]/input[1]"
+ nationality="/html/body/div[1]/div[2]/div/div[2]/div[1]/div[2]/div/div[4]/div/div/input"
+ location="/html/body/div[1]/div[2]/div/div[2]/div[1]/div[2]/div/div[6]/div/input"
+
